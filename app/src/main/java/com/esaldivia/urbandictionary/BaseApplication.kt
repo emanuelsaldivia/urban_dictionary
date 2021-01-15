a3457b79aa2876ca55c0bc6bda563b0c766b65f1
Emanuel Saldivia <emanuel@sima.ag>
2021-01-14 21:07:04 -0300

    app component creation

---
+++ app/src/main/java/com/esaldivia/urbandictionary/BaseApplication.kt
@@ -0,0 +1,13 @@
+package com.esaldivia.urbandictionary
+
+import com.esaldivia.urbandictionary.di.DaggerAppComponent
+import dagger.android.AndroidInjector
+import dagger.android.support.DaggerApplication
+
+class BaseApplication : DaggerApplication() {
+
+    override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
+        return DaggerAppComponent.builder().application(this).build()
+    }
+
+}
